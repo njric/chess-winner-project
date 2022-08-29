f864b1d6330b98eea219c6d87f74d897f29f31a7
Nicolas Richard <njrichard@gmail.com>
2022-08-29 12:22:17 +0200

    new setup from wagon files

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-# setup.py
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
-# list dependencies from file
 with open('requirements.txt') as f:
     content = f.readlines()
-requirements = [x.strip() for x in content]
+requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='chess',
-      description="package for the chess project",
-      packages=find_packages(), # NEW: find packages automatically
-      install_requires=requirements) # NEW
+setup(name='rl-boilerplate',
+      version="1.0",
+      description="Trainer Boilerplate for Reinforcement Learning Projects",
+      packages=find_packages(),
+      install_requires=requirements,
+      include_package_data=True,
+      zip_safe=False)
