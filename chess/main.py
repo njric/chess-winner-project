fd9a5fec3f461219b71f490b1483dd7b8fcd010d
Nicolas Richard <njrichard@gmail.com>
2022-08-30 15:24:01 +0200

    corrected endgame save behaviour

--- chess/main.py
+++ chess/main.py
@@ -4,6 +4,8 @@ from time import sleep
 
 import chess.pgn
 import numpy as np
+import pickle
+
 from pettingzoo.classic import chess_v5
 from pettingzoo.classic.chess import chess_utils
 
@@ -28,18 +30,18 @@ def move_list():
     dir = os.path.join(os.path.dirname(__file__), "../raw_data")
     pgn = bz2.open(f"{dir}/lichess_db_standard_rated_2022-07.pgn.bz2", mode='rt')
 
-    move_list = []
+    moves = []
     for _ in range(10):
         game = chess.pgn.read_game(pgn)
         for move in game.mainline_moves():
-            move_list.append(move)  # .uci())
+            moves.append(move)  # .uci())
             # print(game.mainline())
-        move_list.append(game.headers["Result"])
+        moves.append(game.headers["Result"])
 
-    print(move_list, "\n")
-    return move_list
+    print(moves, "\n")
+    return moves
 
-def load_pgn(move_list):
+def load_pgn(moves):
 
     env = chess_v5.env()
     dat = ({'obs': None, 'act': None},
@@ -47,10 +49,18 @@ def load_pgn(move_list):
 
     env.reset()
 
-    for agent, move in zip(env.agent_iter(), move_list):
+    for agent, move in zip(env.agent_iter(), moves):
 
         agent = int(agent == "player_1")
         new, reward, done, info = env.last()
+
+        if dat[agent]['obs'] is not None:
+
+            old = dat[agent]['obs']
+            act = dat[agent]['act']
+
+            DB.append((old['observation'], act, reward, new['observation']))
+            # print(old.keys(), act, reward, new.keys())
 
         if type(move) is str:
             reward = score(move)
@@ -59,14 +69,6 @@ def load_pgn(move_list):
             print("Game done", reward, "\n\n")
             env.reset()
             continue
-
-        if dat[agent]['obs'] is not None:
-
-            old = dat[agent]['obs']
-            act = dat[agent]['act']
-
-            DB.append((old, act, reward, new))
-            # print(old.keys(), act, reward, new.keys())
 
         if agent == 1:
             move = chess_utils.mirror_move(move)
@@ -84,3 +86,4 @@ def load_pgn(move_list):
 moves = move_list()
 load_pgn(moves)
 print(len(DB))
+print(DB[0])
