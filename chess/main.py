9ab539f69d4e37a81c9156b494ccc808708cb4fd
Nicolas Richard <njrichard@gmail.com>
2022-08-30 15:06:23 +0200

    consolidated main

--- chess/main.py
+++ chess/main.py
@@ -7,6 +7,7 @@ import numpy as np
 from pettingzoo.classic import chess_v5
 from pettingzoo.classic.chess import chess_utils
 
+import bz2
 
 def move_to_act(move):
     x, y = chess_utils.square_to_coord(move.from_square)
@@ -23,50 +24,63 @@ def score(move: str): # -> score
 
 DB = []
 
-def load_pgn():
+def move_list():
+    dir = os.path.join(os.path.dirname(__file__), "../raw_data")
+    pgn = bz2.open(f"{dir}/lichess_db_standard_rated_2022-07.pgn.bz2", mode='rt')
+
+    move_list = []
+    for _ in range(10):
+        game = chess.pgn.read_game(pgn)
+        for move in game.mainline_moves():
+            move_list.append(move)  # .uci())
+            # print(game.mainline())
+        move_list.append(game.headers["Result"])
+
+    print(move_list, "\n")
+    return move_list
+
+def load_pgn(move_list):
 
     env = chess_v5.env()
-    pgn = open(os.path.join(os.path.dirname(__file__), "2022-01.bare.[19999].pgn"))
     dat = ({'obs': None, 'act': None},
            {'obs': None, 'act': None})
 
-    while (game := chess.pgn.read_game(pgn)) is not None:
+    env.reset()
 
-        env.reset()
+    for agent, move in zip(env.agent_iter(), move_list):
 
-        for agent, move in zip(env.agent_iter(), game.mainline_moves()):
+        agent = int(agent == "player_1")
+        new, reward, done, info = env.last()
 
-            agent = int(agent == "player_1")
-            new, rwd, done, info = env.last()
+        if type(move) is str:
+            reward = score(move)
+            done = True
+        if done:
+            print("Game done", reward, "\n\n")
+            env.reset()
+            continue
 
-            if type(move) is str:
-                reward = score(move)
-                done = True
-            if done:
-                print("Game done", reward, "\n\n")
-                env.reset()
-                continue
+        if dat[agent]['obs'] is not None:
 
-            if dat[agent]['obs'] is not None:
+            old = dat[agent]['obs']
+            act = dat[agent]['act']
 
-                old = dat[agent]['obs']
-                act = dat[agent]['act']
+            DB.append((old, act, reward, new))
+            # print(old.keys(), act, reward, new.keys())
 
-                DB.append((old, act, rwd, new))
-                print(old.keys(), act, rwd, new.keys())
-
-            if agent == 1:
-                move = chess_utils.mirror_move(move)
-            action = move_to_act(move)
-            env.step(action)
+        if agent == 1:
+            move = chess_utils.mirror_move(move)
+        action = move_to_act(move)
+        env.step(action)
 
 
-            dat[agent]['obs'] = new
-            dat[agent]['act'] = action
+        dat[agent]['obs'] = new
+        dat[agent]['act'] = action
 
-            if done:
-                break
+    print("Done")
 
-        print("Done")
 
-load_pgn()
+
+moves = move_list()
+load_pgn(moves)
+print(len(DB))
